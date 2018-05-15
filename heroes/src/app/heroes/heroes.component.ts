995ef987761bcbebca371b85b46fa98262faa74f
Kate-M <ekaterina.minich@gmail.com>
2018-05-15 15:31:09 +0300

    feature: added some styles, implemented functionality for select hero

--- heroes/src/app/heroes/heroes.component.ts
+++ heroes/src/app/heroes/heroes.component.ts
@@ -5,17 +5,20 @@ import { HEROES } from './mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
   heroes = HEROES;
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.selectedHero);
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(this.selectedHero.name);
+  }
 }
--- heroes/src/app/heroes/mock-heroes.ts
+++ heroes/src/app/heroes/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { age: 54, name: 'Jeff Bezos', money: '112 B'},
+  { age: 62, name: 'Bill Gates', money: '90 B'},
+  { age: 84, name: 'Warren Buffett', money: '87 B'},
+  { age: 69, name: 'Bernard Arnault', money: '72 B'},
+  { age: 34, name: 'Mark Zuckerberg', money: '71 B'},
+  { age: 82, name: 'Amancio Ortega', money: '70 B'},
+  { age: 78, name: 'Carlos Slim Helu', money: '67.1 B'},
+  { age: 82, name: 'Charles Koch', money: '60 B'},
+  { age: 78, name: 'David Koch', money: '60 B'},
+  { age: 73, name: 'Larry Ellison', money: '58.5 B'},
 ];
