d70f6508202001ced184fa28a263ad181a2956ba
Kate-M <ekaterina.minich@gmail.com>
2018-05-16 17:23:18 +0300

    fix:fixed info Top 3

--- heroes/src/app/dashboard/dashboard.component.ts
+++ heroes/src/app/dashboard/dashboard.component.ts
@@ -17,7 +17,7 @@ export class DashboardComponent implements OnInit {
   }
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes.slice(1, 4));
+        .subscribe(heroes => this.heroes = heroes.slice(0, 3));
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
